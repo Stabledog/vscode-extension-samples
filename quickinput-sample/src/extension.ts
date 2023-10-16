1cbaa0cf7038776a0aa523fb7d0fac4bcf154ed6
Les Matheson <lmatheson4@bloomberg.net>
2023-10-15 20:15:20 -0400

    Rework quickPickMenu to distinguish between menu  labels and func names

--- quickinput-sample/src/extension.ts
+++ quickinput-sample/src/extension.ts
@@ -3,28 +3,36 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { window, commands, ExtensionContext } from 'vscode';
+import { window, commands, ExtensionContext, QuickPickItem } from 'vscode';
+//import { QuickPickItem } from 'vscode';
 import { showQuickPick, showInputBox } from './basicInput';
 import { multiStepInput } from './multiStepInput';
 import { quickOpen } from './quickOpen';
 
+interface MenuItem extends QuickPickItem {
+    func: (arg:ExtensionContext) => Promise<void>;
+}
+
 export function activate(context: ExtensionContext) {
-	context.subscriptions.push(commands.registerCommand('samples.quickInput', async () => {
-		const options: { [key: string]: (context: ExtensionContext) => Promise<void> } = {
-			showQuickPick,
-			showInputBox,
-			multiStepInput,
-			quickOpen,
-		};
-		const quickPick = window.createQuickPick();
-		quickPick.items = Object.keys(options).map(label => ({ label }));
-		quickPick.onDidChangeSelection(selection => {
-			if (selection[0]) {
-				options[selection[0].label](context)
-					.catch(console.error);
-			}
-		});
-		quickPick.onDidHide(() => quickPick.dispose());
-		quickPick.show();
-	}));
+    context.subscriptions.push(commands.registerCommand('samples.quickInput', async () => {
+        const options: MenuItem[] = [
+            { label: "Show quick pick", func: showQuickPick },
+            { label: "Show input box", func: showInputBox },
+            { label: "Multi-step input", func: multiStepInput },
+            { label: "Quick open", func: quickOpen },
+        ];
+        const quickPick = window.createQuickPick();
+        //quickPick.items = options.map(opt => ({ label: opt.optName, }));
+        quickPick.items = options;
+        quickPick.onDidChangeSelection(selection => {
+            console.log(selection);
+            if (selection[0]) {
+                console.log(selection[0]);
+                (selection[0] as MenuItem).func(context)
+                    .catch(console.error);
+            }
+        });
+        quickPick.onDidHide(() => quickPick.dispose());
+        quickPick.show();
+    }));
 }
